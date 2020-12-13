e654af8dd985fbae64b75eba811234ae35afbf20
ReedhamHackerman <48519104+ReedhamHackerman@users.noreply.github.com>
2020-12-13 01:49:58 -0500

    Some Major Planning
    Planning , Action , World , Actions ,Agents

---
+++ Assets/Prefab/Scripts/GAction.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class GAction : MonoBehaviour
+{
+
+    public string actionName = "Action";
+    public float cost = 1.0f;
+    public GameObject target;
+    public GameObject targetTag;
+    public float duration = 0;
+    public WorldState[] preConditions;
+    public WorldState[] afterEffects;
+    public NavMeshAgent agent;
+
+
+    public Dictionary<string, int> preconditions;
+    public Dictionary<string, int> effects;
+
+
+
+    public WorldStates agentBeliefs;
+    public bool running = false;
+    public GAction()
+    {
+        preconditions = new Dictionary<string, int>();
+        effects = new Dictionary<string, int>();
+    }
+
+
+    public void Awake()
+    {
+        agent = this.gameObject.GetComponent<NavMeshAgent>();
+        if(preConditions!=null)
+        {
+            foreach (WorldState worldState in preConditions)
+            {
+                preconditions.Add(worldState.key, worldState.value);
+            }
+        }
+        if (afterEffects != null)
+        {
+            foreach(WorldState worldState in afterEffects)
+            {
+                effects.Add(worldState.key, worldState.value);
+            }
+        }
+
+
+    }
+    
+
+    public bool IsAchievable()
+    {
+        return true;
+    }
+
+    public bool IsAchievableGiven(Dictionary<string,int> conditions)
+    {
+        foreach (KeyValuePair<string,int> p  in preconditions)
+        {
+            if(!conditions.ContainsKey(p.Key))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public abstract bool PrePerform();
+    public abstract bool PostPerform();
+}
---
+++ Assets/Prefab/Scripts/GPlanner.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Node
+{
+    public Node parent;
+    public float cost;
+    public Dictionary<string, int> states;
+    public GAction action;
+    public Node(Node parent,float cost , Dictionary<string,int> allstates,GAction action)
+    {
+        this.parent = parent;
+        this.cost = cost;
+        this.states = new Dictionary<string, int>(allstates);
+        this.action = action;
+    }
+}
+
+public class GPlanner
+{
+    public Queue<GAction> plan(List<GAction> actions,Dictionary<string,int> goal,WorldStates states)
+    {
+        List<GAction> usableActions = new List<GAction>();
+        foreach(GAction g in actions)
+        {
+            if(g.IsAchievable())
+            {
+                usableActions.Add(g);
+            }
+        }
+        List<Node> leaves = new List<Node>();
+        Node start = new Node(null,0, GWorld.Instance.GetWorld().GetStates(), null);
+        bool success = BuildGraph(start, leaves, usableActions, goal);
+        if(!success)
+        {
+            Debug.Log("No Plan");
+            return null;
+        }
+        Node cheapest = null;
+        foreach (Node leaf in leaves)
+        {
+            if(cheapest == null)
+            {
+                cheapest = leaf;
+            }
+            else
+            {
+                if(leaf.cost <cheapest.cost)
+                {
+                    cheapest = leaf;
+                }
+            }
+        }
+
+        List<GAction> result = new List<GAction>();
+        Node n = cheapest;
+        while(n != null)
+        {
+            if(n.action != null)
+            {
+                result.Insert(0, n.action);
+            }
+            n = n.parent;
+        }
+        Queue<GAction> queue = new Queue<GAction>();
+        foreach (GAction a in result)
+        {
+            queue.Enqueue(a);
+        }
+        Debug.Log("The Plan is : ");
+        foreach (GAction a in queue)
+        {
+            Debug.Log("Q: " + a.actionName);
+        }
+        return queue;
+    }
+}
+   
